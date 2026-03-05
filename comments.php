51dabcb2d39c7d11a96f433bf296315873615d59
Prianto Dey <priantodey12@gmail.com>
2026-03-06 00:24:14 +0600

    initila commit

---
+++ comments.php
@@ -0,0 +1,26 @@
+<?php
+$occupation_prianto = "full time bekar ";
+$occupation_anik = "programmer ";
+$occupation_dhiraj = "d jango ";
+$occupation_mahan = "ui/ux designer";
+
+ 
+echo "Prianto's occupation is ". $occupation_prianto ."\n";
+echo "Anik's occupation is". $occupation_anik ."\n";
+echo "Dhiraj's occupation is". $occupation_dhiraj ."\n";
+echo "Mahan's occupation is". $occupation_mahan ."\n" ;
+
+
+
+//this is a single line commnet in php
+#this is also a single line comment in php
+/*
+this is a multiline 
+comment in php
+=============
+php is quite similar with java ,don't know why but it seems to me .
+
+*/
+
+
+?>
---
+++ variables.php
@@ -0,0 +1,10 @@
+<?php 
+$name = "jayanto dey";
+$age=22;
+$float_val= 23.33;
+
+
+echo "name : ".$name."\nage :".$age;
+echo "\nfloat val".$float_val;
+
+?>
