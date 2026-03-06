54c00b4462783ef0777e6851a2357886e053f1d7
Prianto Dey <priantodey12@gmail.com>
2026-03-06 20:36:27 +0600

    associative array for api

---
+++ associative_array.php
@@ -0,0 +1,11 @@
+<?php
+$user = [
+    "name" => "Prianto",
+    "email" => "xyz@gmail.com",
+    "phone" => "016743799231"
+];
+
+echo $user["name"]."\n";
+echo $user["email"]."\n";
+echo $user["phone"];
+?>
