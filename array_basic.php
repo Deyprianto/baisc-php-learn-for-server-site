67d0687d20ea6fadf95739eb242e9a247f8cffd3
Prianto Dey <priantodey12@gmail.com>
2026-03-06 20:23:02 +0600

    array in php

---
+++ array_basic.php
@@ -0,0 +1,10 @@
+<?php 
+$fruits = ["apple" ,"banana","mango","orange","pinapple"];
+
+#echo $fruits[0]."\n";
+
+for($i=0;$i<count($fruits);$i++) {
+    echo $fruits[$i]."\n";
+}
+echo count($fruits);
+?>
