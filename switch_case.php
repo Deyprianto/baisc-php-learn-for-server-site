f94d70e1df70a4338896fc928f526c8407886397
Prianto Dey <priantodey12@gmail.com>
2026-03-06 13:14:38 +0600

    switch case in php

---
+++ switch_case.php
@@ -0,0 +1,16 @@
+<?php 
+$day = 3;
+//$day = 2;
+
+
+switch($day){
+    case 1:
+        echo "today is saturday";
+        break;
+    case 2:
+        echo "today is sunday ";
+        break;  
+    default:
+        echo "today is not saturday and sunday";      
+}
+?>
