8155ef3a06e2d448081dca1ff3bd004eefc96fac
Prianto Dey <priantodey12@gmail.com>
2026-03-09 01:26:46 +0600

    basic function in php

---
+++ function_basic.php
@@ -0,0 +1,12 @@
+<?php
+function info($name,$age){
+    echo "Your name is ".$name." and your age is ".$age."\n";
+}
+function  information($nationalty){
+    echo "Your nationality is ".$nationalty."\n";
+}
+info("Alu", 25);
+information("Bangladeshi");
+
+
+?>
