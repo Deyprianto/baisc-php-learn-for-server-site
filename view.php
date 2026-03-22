d0a161056296433b6007dfa1807b633d4671a6af
prianto dey <149568690+Deyprianto@users.noreply.github.com>
2026-03-22 21:17:33 +0600

    Add view.php to return user information as JSON

---
+++ view.php
@@ -0,0 +1,12 @@
+<?php 
+
+$user_info = array(
+        "name" => "Jayanto",
+        "mobile" => "016430294115",
+        "email" => "jayantodey@gmail.com",
+        "address" => "sylhet"
+);
+
+echo json_encode($user_info);
+
+?>
